37df3bbf26615a53335a8859340af9c0e554a9f9
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-08-06 13:23:06 +0300

    Adjusted entity and added Environment package and classes without implementation.

--- Engine/src/objects/Entity.java
+++ Engine/src/objects/Entity.java
@@ -2,8 +2,55 @@ package objects;
 
 import properties.Property;
 
+import java.util.Map;
+
 public class Entity {
+
+    private int population;
     private String name;
-    private Property<?>[] properties;
-    private Rule[] rules;
+    private Map<Integer, Property<?>> properties;
+
+    public Entity(int population, String name, Map<Integer, Property<?>> properties) {
+        this.population = population;
+        this.name = name;
+        this.properties = properties;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<Integer, Property<?>> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Map<Integer, Property<?>> properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder entityToString = new StringBuilder("Entity{" +
+                "Name='" + name + '\'' +
+                ", Population='" + '\'' +
+                ",Properties='");
+        for (Property<?> p : properties.values()
+        ) {
+            entityToString.append(p.toString());
+        }
+        entityToString.append("'}");
+        return entityToString.toString();
+    }
 }
---
+++ Engine/src/objects/env/EnvRule.java
@@ -0,0 +1,4 @@
+package objects.env;
+
+public class EnvRule {
+}
