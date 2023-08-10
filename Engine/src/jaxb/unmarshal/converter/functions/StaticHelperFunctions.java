53b45ad9255b1f59a95d3cd8c5ce55f9237b3a08
Roy Toledano <royto@mta.ac.il>
2023-08-10 19:01:34 +0300

    add documentation

--- Engine/src/jaxb/unmarshal/converter/functions/StaticHelperFunctions.java
+++ Engine/src/jaxb/unmarshal/converter/functions/StaticHelperFunctions.java
@@ -7,13 +7,29 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
+/**
+ * A Static class implements five functions which can be requested as a value for an action object.
+ */
 public class StaticHelperFunctions {
 
+    /**
+     * Receive an environment variable's name and return its value.
+     *
+     * @param environmentVarName the given environment variable name.
+     * @param environmentVars the world's environment variables map.
+     * @return the given environment variable's value.
+     */
     static public Object environment(String environmentVarName, Map<String,Property> environmentVars) {
         Property environmentVar = environmentVars.get(environmentVarName);
         return environmentVar.getValue();
     }
 
+    /**
+     * Receive a number and random an integer between 0 and this number (inclusive).
+     *
+     * @param range the upper limit of the range.
+     * @return the random number.
+     */
     static public int random(int range){
         Random random = new Random();
         // TODO : check if range is a number.
