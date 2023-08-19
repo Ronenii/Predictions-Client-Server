ab407e1ddf4da2b08cdd057e835ee878944be8a6
Roy Toledano <royto@mta.ac.il>
2023-08-19 16:30:02 +0300

    the simulation runs without crashing

--- Engine/src/simulation/properties/action/impl/condition/ConditionOperator.java
+++ Engine/src/simulation/properties/action/impl/condition/ConditionOperator.java
@@ -3,13 +3,15 @@ package simulation.properties.action.impl.condition;
 import java.util.Objects;
 
 public enum ConditionOperator {
-    EQUALS, NOT_EQUALS, BIGGER_THAN, LESSER_THAN;
+    EQUALS, NOT_EQUALS, BIGGER_THAN, LESSER_THAN, AND, OR;
 
     public static ConditionOperator tryParse(String operatorString){
         if(Objects.equals(operatorString, "bt")){return BIGGER_THAN;}
         if(Objects.equals(operatorString, "lt")) {return  LESSER_THAN;}
         if(Objects.equals(operatorString, "=")) { return  EQUALS;}
         if(Objects.equals(operatorString, "!=")) { return NOT_EQUALS;}
+        if(Objects.equals(operatorString, "and")) {return AND;}
+        if(Objects.equals(operatorString, "or")) {return OR;}
 
         throw new IllegalArgumentException("Invalid operator string");
     }
