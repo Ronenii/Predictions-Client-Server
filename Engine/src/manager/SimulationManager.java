c9e0609d93454a93169b38376209ba59a3f5614b
Roy Toledano <royto@mta.ac.il>
2023-09-13 17:19:15 +0300

    finished

--- Engine/src/manager/SimulationManager.java
+++ Engine/src/manager/SimulationManager.java
@@ -361,6 +361,9 @@ public class SimulationManager implements EngineInterface, Serializable {
     }
 
     private void addSimulationToQueue() {
-        simulation.runSimulation();
+        new Thread(() -> {
+            simulation.runSimulation();
+        }).start();
+
     }
 }
