d578991e91467b03b0cc00fe3e643ee8023f6350
Roy Toledano <royto@mta.ac.il>
2023-09-18 09:48:29 +0300

    fix the problem

--- UI/src/gui/result/details/control/bar/ExecutionDetailsControlBarController.java
+++ UI/src/gui/result/details/control/bar/ExecutionDetailsControlBarController.java
@@ -34,6 +34,8 @@ public class ExecutionDetailsControlBarController {
         mainController.setExecutionQueueTaskOnPause();
         if(isPauseFirstClick){
             isPauseFirstClick = false;
+            // For the first click on pause we allow the task in the execution queue controller (which fetches the runtime & result details components)
+            // to complete another loop, and then we put this loop on hold.
             mainController.setOneUpdateAfterPauseFlag();
         }
         skipForwardBTN.setDisable(false);
@@ -62,6 +64,9 @@ public class ExecutionDetailsControlBarController {
     void skipForwardButtonActionListener(ActionEvent event) {
         if(isSkipForwardFirstClick){
             isSkipForwardFirstClick = false;
+            // For the first click on skip forward we create the task in the execution details component (which informs the engine moving one tick forward),
+            // and after one update of the engine, the task will be put on hold until another click on the
+            // skip forward button (which will go to the 'else' and allow the task run one loop).
             mainController.sendSkipForwardToTheEngine();
         } else {
             mainController.setSkipOne(true);
