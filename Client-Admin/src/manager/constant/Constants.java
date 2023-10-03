8fe05b0f6a5acffed6d08a7260b5d203fee7c8ae
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-10-03 16:19:57 +0300

    - Added more constants to Constants.java

--- Client-Admin/src/manager/constant/Constants.java
+++ Client-Admin/src/manager/constant/Constants.java
@@ -2,9 +2,18 @@ package manager.constant;
 
 public class Constants {
 
+    // global constants
+
+    public final static int REFRESH_RATE = 2000;
+
     // Server resource locations
-    public final static String BASE_DOMAIN = "localhost";
+    private final static String BASE_DOMAIN = "localhost";
 
-    public final static String BASE_URL = "http://" + BASE_DOMAIN;
+    private final static String BASE_URL = "http://" + BASE_DOMAIN;
 
+    private final static String CONTEXT_PATH = "/Predictions_Web";
+
+    private final static String FULL_SERVER_PATH = BASE_URL + CONTEXT_PATH;
+
+    public final static String ADMIN_CONNECT_PATH = FULL_SERVER_PATH + "/adminConnect";
 }
