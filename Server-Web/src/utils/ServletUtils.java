b81e6fe5496bc623bfe30da5b953731a263ffbbd
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-10-05 16:14:13 +0300

    - Adjusted dependency shit - Created serverUtils.java and UserManager.java.

---
+++ Server-Web/src/utils/ServletUtils.java
@@ -0,0 +1,29 @@
+package utils;
+
+import manager.SimulationManager;
+import user.UserManager;
+import jakarta.servlet.ServletContext;
+
+public class ServletUtils {
+    private static final String USER_MANAGER_ATTRIBUTE_NAME = "userManager";
+    private static final String SIMULATION_MANAGER_ATTRIBUTE_NAME = "simulationManager";
+    private static final Object userManagerLock = new Object();
+    private static final Object simulationManagerLock = new Object();
+    public static UserManager getUserManager(ServletContext servletContext) {
+
+        synchronized (userManagerLock) {
+            if (servletContext.getAttribute(USER_MANAGER_ATTRIBUTE_NAME) == null) {
+                servletContext.setAttribute(USER_MANAGER_ATTRIBUTE_NAME, new UserManager());
+            }
+        }
+        return (UserManager) servletContext.getAttribute(USER_MANAGER_ATTRIBUTE_NAME);
+    }
+    public static SimulationManager getSimulationManager(ServletContext servletContext) {
+
+        synchronized (simulationManagerLock) {
+            if (servletContext.getAttribute(SIMULATION_MANAGER_ATTRIBUTE_NAME) == null) {
+                servletContext.setAttribute(SIMULATION_MANAGER_ATTRIBUTE_NAME, new SimulationManager());
+            }
+        }
+        return (SimulationManager) servletContext.getAttribute(USER_MANAGER_ATTRIBUTE_NAME);
+    }}
