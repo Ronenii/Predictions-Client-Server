38d4aecb4f653813c426128f099ce04a3e97b22c
Ronen Gelmanovich <ronenge@mta.ac.il>
2023-12-23 13:57:13 +0200

    - holds requested by branch contents

--- Server-Web/src/servlets/admin/SendSimulationsAddedServlet.java
+++ Server-Web/src/servlets/admin/SendSimulationsAddedServlet.java
@@ -24,6 +24,7 @@ public class SendSimulationsAddedServlet extends HttpServlet {
         String cookieSavedVersion = CookiesUtils.getSavedValueOnCookie(req, Constants.SIMULATIONS_ADDED);
         SimulationManager simulationManager = ServletUtils.getSimulationManager(getServletContext());
 
+
         if(cookieSavedVersion != null){
             simulationsAdded = Integer.parseInt(cookieSavedVersion);
         }
